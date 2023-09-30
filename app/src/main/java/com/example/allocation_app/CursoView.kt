8ef345ae111ca9ec11fae23c8af183d2afa7acf9
Jacob Berenstein Neto <jbnlivriaria@gmail.com>
2023-09-29 23:54:03 -0300

    Screen layout update  tela 2

---
+++ app/src/main/java/com/example/allocation_app/CursoView.kt
@@ -0,0 +1,11 @@
+package com.example.allocation_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CursoView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_curso_view)
+    }
+}
--- app/src/main/java/com/example/allocation_app/Home.kt
+++ app/src/main/java/com/example/allocation_app/Home.kt
@@ -3,10 +3,10 @@ package com.example.allocation_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
 
     }
 }
