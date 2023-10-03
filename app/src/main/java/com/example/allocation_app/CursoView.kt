53300c950f02aba3065356bcd65a5cd04f7835ba
Jacob Berenstein Neto <jbnlivriaria@gmail.com>
2023-10-02 22:29:35 -0300

    update layout tela2 and recycler View config

--- app/src/main/java/com/example/allocation_app/CursoView.kt
+++ app/src/main/java/com/example/allocation_app/CursoView.kt
@@ -2,10 +2,21 @@ package com.example.allocation_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class CursoView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_curso_view)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view_registered)
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        val itens = listOf("teste 1", "Item 2", "Item 3") // Substitua isso com a lista de itens que você deseja exibir
+        val meuAdaptador = MeuAdaptador(itens)
+        recyclerView.adapter = meuAdaptador
+
     }
 }
